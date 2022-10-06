c7052f62479c4db55801824dc2a6052fcfd80f58
a401691 <gayathri.yashas@gmail.com>
2022-10-06 09:31:07 -0400

    PADashboardShortcuts

---
+++ src/test/java/com/volvo/project/pages/PADashboardShortcuts.java
@@ -0,0 +1,16 @@
+package com.volvo.project.pages;
+
+import com.volvo.project.components.PageObject;
+import org.openqa.selenium.WebDriver;
+
+public class PADashboardShortcuts extends PageObject
+{
+
+
+    public PADashboardShortcuts(WebDriver driver)
+    {
+        super(driver);
+    }
+
+
+}
